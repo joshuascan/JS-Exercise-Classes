637ccb12dbf043272b981131ba480d2928e51819
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-04-22 14:34:18 -0700

    Completed task 5

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- 
+
  class Instructor extends Lambdasian {
      constructor(attr) {
          super(attr);
@@ -174,8 +174,23 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ 
+ class Student extends Lambdasian {
+     constructor(attr) {
+         super(attr);
+         this.previousBackground = attr.previousBackground;
+         this.className = attr.className;
+         this.favSubjects = attr.favSubjects;
+     }
+     listSubjects() {
+         return this.favSubjects.toString();
+     }
+     PRAssignment(subject) {
+         return `${this.name} has submitted a PR for ${subject}`;
+     }
+     sprintChallenge(subject) {
+         return `${this.name} has begun sprint challenge on ${subject}`;
+     }
  }
   
   /*
