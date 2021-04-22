ab5f878a60b56c891b1347b18aa40606181c199c
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-04-22 14:19:42 -0700

    Completed task 4

--- index.js
+++ index.js
@@ -119,10 +119,10 @@ class Airplane {
   */
 
  class Lambdasian {
-    constructor(object) {
-        this.name = object.name;
-        this.age = object.age;
-        this.location = object.location;
+    constructor(attr) {
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
     }
     speak() {
         return `Hello my name is ${this.name}, I am from ${this.location}`;
@@ -143,9 +143,22 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ 
+ class Instructor extends Lambdasian {
+     constructor(attr) {
+         super(attr);
+         this.specialty = attr.specialty;
+         this.favLanguage = attr.favLanguage;
+         this.catchPhrase = attr.catchPhrase;
+     }
+     demo(subject) {
+         return `Today we are learning about ${subject}`;
+     }
+     grade(student, subject) {
+         return `${student.name} receives a perfect score on ${subject}`;
+     }
  }
+
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
